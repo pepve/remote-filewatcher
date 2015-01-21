73bfab9c2620a265d1c0ca0407d473c1560be342
Pepijn Verlaan <pepijnverlaan@yahoo.com>
2015-01-21 12:12:42 +0100

    Fix wire protocol for commands without arguments

--- test/simple.js
+++ test/simple.js
@@ -5,8 +5,14 @@ var assert = require('assert'),
 
 var expectedFile = __dirname + '/foo';
 var actualFile;
+var serverRanToEnd;
 
 var server = child_process.spawn('node', [__dirname + '/../lib/server.js', 'localhost', '12345'], { stdio: 'inherit' });
+var serverExited = false;
+
+server.on('exit', function() {
+	serverExited = true;
+});
 
 setTimeout(function () {
 	var client = remoteFilewatcher({ host: 'localhost', port: '12345' });
@@ -21,16 +27,20 @@ setTimeout(function () {
 
 		client.on('change', function (file) {
 			actualFile = file;
+
+			client.removeAll();
 		});
 	}, 1000);
 }, 1000);
 
 setTimeout(function () {
+	serverRanToEnd = !serverExited;
 	server.kill();
 }, 4000);
 
 process.on('exit', function() {
 	fs.unlinkSync(expectedFile);
+	assert.ok(serverRanToEnd);
 	assert.equal(actualFile, expectedFile);
 	console.log('Simple test OK');
 });
